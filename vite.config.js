c6f91bf21fa94f149c07e1a0cbc066e207d21154
Christian Santanas <50605403+chris171290@users.noreply.github.com>
2024-01-15 20:18:01 -0500

    build(gh-pages): config gh-pages

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 3006,
-  },
+  base: '/restaurant-business-template/'
 })
